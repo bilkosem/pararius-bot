c15c5ecc5ed5b0a1ff2d27f69224a687cfac52f8
Bilgehan Kösem <bilkos92@gmail.com>
2023-05-31 00:17:40 +0200

    update connectivity bot; add telegram_interface module; update chromedrivers; add logging

--- pararius-bot.py
+++ pararius-bot.py
@@ -3,46 +3,54 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import time
-from connectivity_bot.TelegramHandler import *
+from connectivity_bot.telegram_wrapper import *
+from logging.handlers import TimedRotatingFileHandler
 import os
 import signal
+import telegram_interface
+import itertools
+
 cached_addresses = []
 
 main_url = "https://www.pararius.com/apartments"
 is_bot_enabled = True
 is_first_cycle = True
 
-def configure_telegrambot(token, chat_id):
-    TelegramBot.setToken(token)
-    TelegramBot.setChatId(chat_id)
+def setup_logger(logger, log_config):
 
-    TelegramBot.add_handler(CommandHandler('start', start), description="start application")
-    TelegramBot.add_handler(CommandHandler('stop', stop), description="stop application")
-    TelegramBot.add_handler(CommandHandler('kill', TelegramBot.kill), description="kill application")
-    TelegramBot.add_handler(CommandHandler('help', TelegramBot.help), description="")
-    TelegramBot.add_handler(MessageHandler(Filters.text, unknown))
-    TelegramBot.add_handler(MessageHandler(Filters.command, unknown)) 
-    TelegramBot.add_handler(MessageHandler(Filters.text, unknown_text))
+    log_dir = os.path.dirname(log_config['file'])
+    if not os.path.isdir(log_dir):
+        os.makedirs(log_dir, exist_ok=True)
 
-    # How to get chatId: https://api.telegram.org/bot<YourBOTToken>/getUpdates
+    else:
+        if log_config.get('clear',False) and os.path.isfile(log_config['file']) :
+            os.remove(log_config['file'])
 
-    format = TelegramMessageFormat('Header','\ntailer','\n        ','/{}: {}')
-    TelegramBot.add_format('help', format, constant_data=TelegramBot.command_desc)
-    #TelegramBot.send_formatted_message('help')
-    new_advert_format = TelegramMessageFormat('New advert found','','\n        ','{}: {}')
-    TelegramBot.add_format('new_advert',new_advert_format)
+    logger = logging.getLogger(log_config.get('logger_name','pararius'))
+    logger.setLevel(logging.DEBUG)
 
+    rfh = TimedRotatingFileHandler(filename=log_config['file'],
+                                   when='D',
+                                   interval=1,
+                                   backupCount=30)
 
-def start(update: Update, context: CallbackContext):
-    global is_bot_enabled
-    update.message.reply_text("Starting the bot...")
-    is_bot_enabled = True
+    rfh.setLevel(log_config['level'])
 
+    # create console handler with a higher log level
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.WARNING)
 
-def stop(update: Update, context: CallbackContext):
-    global is_bot_enabled
-    update.message.reply_text("Stopping the bot...")
-    is_bot_enabled = False
+    # create formatter and add it to the handlers
+    formatter = logging.Formatter('[{}][{}][{} - {}][{}][{}]'.format('%(asctime)s',
+        '%(filename)-21s','%(lineno)-3d','%(funcName)-24s','%(levelname)8s', '%(message)s'))
+    formatter.converter = time.gmtime # Use the UTC Time
+    rfh.setFormatter(formatter)
+    ch.setFormatter(formatter)
+
+    logger.addHandler(rfh)
+    logger.addHandler(ch)
+
+    logger.info('logger has been set')
 
 
 def sleep_for_minutes(minute):
@@ -53,35 +61,35 @@ def sleep_for_minutes(minute):
 
 
 def browse_query(query):
-    global is_first_cycle
+    global is_first_cycle, cached_addresses
     driver.get(query)
+
+    # Delay for initial loading
+    time.sleep(1)
 
     # Check if the captcha page is displayed
     is_captcha = driver.find_elements(By.ID, "_csnl_cp")
     if is_captcha:
+        logger.error('Hit to Captcha !!!')
         TelegramBot.send_raw_message('Hit to Captcha !!!')
         raise ValueError('Hit to Captcha !!!')
 
+    advertisements = []
     results = driver.find_elements(By.XPATH, "//span[@class='listing-label listing-label--new']")
     for result in results:
-        adress = result.find_element(By.XPATH, "./../../div[@class='listing-search-item__location']").text
+        adress = result.find_element(By.XPATH, "./../../div[@class=\"listing-search-item__sub-title'\"]").text
         advert_url = result.find_element(By.XPATH, "./../../h2[@class='listing-search-item__title']/a").get_attribute('href')
         advert_price = result.find_element(By.XPATH, "./../../div[@class='listing-search-item__price']").text
         advert_feature = result.find_element(By.XPATH, "./../../div[@class='listing-search-item__features']/ul").text.replace('\n',' | ')
-        print(f"Address found {adress}")
 
         if adress in cached_addresses:
             continue
         else:
             message_dict = {'Address': adress, 'Price': advert_price, 'Feature': advert_feature, 'Link':advert_url}
-            print(message_dict)
-
-            # Do not send notification for the first cycle
-            if is_first_cycle:
-                is_first_cycle = False
-            else:
-                TelegramBot.send_formatted_message('new_advert', message_dict)
-                cached_addresses.append(adress)
+            logger.info('New advertisement found: {}'.format(message_dict))
+            advertisements.append(message_dict)
+            cached_addresses.append(adress)
+    return advertisements
 
 
 def build_queries():
@@ -104,50 +112,76 @@ def build_queries():
 
 
 def application():
+    global is_first_cycle
     queries = build_queries()
-    print(queries)
+    logger.debug(queries)
+
+    browsed_advertisements = []
     for query in queries:
-        browse_query(query)
+
+        advertisements = browse_query(query)
+        browsed_advertisements.append(advertisements)
+
+    browsed_ad_list = list(itertools.chain(*browsed_advertisements))
+
+    if is_first_cycle:
+        is_first_cycle = False
+        logger.info('First cycle completed. Number of new advertisements that are just cached: {}'.format(len(browsed_ad_list)))
+        return
+
+    for ad in browsed_ad_list:
+        TelegramBot.send_formatted_message('new_advert', ad)
+        time.sleep(0.5)
+    
+    return  
 
 
 if __name__ == "__main__":
-    # TODO: Add logging
+
     f = open(str(sys.argv[1]),'r')
     main_config = json.load(f)
+
+    logger = logging.getLogger('pararius')
+    setup_logger(logger, main_config['log'])
 
     f = open(main_config['telegram_config'],'r')
     telegram_config = json.load(f)
 
-    configure_telegrambot(telegram_config['token'], telegram_config['chat_id'])
-    TelegramBot.updater.start_polling()
-    
-    chrome_options = Options()
-    #chrome_options.add_experimental_option("detach", True)
+    telegram_interface.init_telegram_bot(telegram_config['token'], telegram_config['chat_id'])
+    telegram_interface.start_telegram_bot()
 
+    chrome_options = Options()
     if not main_config['enable_display']:
-        chrome_options.add_argument('--headless')
-        chrome_options.add_argument("--remote-debugging-port=9222")  
-    #from webdriver_manager.chrome import ChromeDriverManager
-    #driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+        chrome_options.add_argument('--headless=new')
+        #chrome_options.add_argument("--remote-debugging-port=9222")  
+
+    chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
     driver = webdriver.Chrome(main_config['chrome_driver'], options=chrome_options)
     while(True):
-        print('starting a cycle')
+        logger.debug('Iteration started')
+        
         try:
             if not is_bot_enabled:
                 continue
 
+
+            t = time.localtime()
+            current_time = time.strftime("%H:%M:%S", t)
+            print("Current Time:", current_time)
+                
             t = time.localtime()
             current_time = time.strftime("%H:%M:%S", t)
             print("Current Time:", current_time)
             application()
-                
+            driver.delete_all_cookies()
             sleep_for_minutes(main_config['polling_interval'])
+
         except ValueError as e:
             print(e)
             break
         except KeyboardInterrupt:
-            TelegramBot.updater.stop()
             break
 
-    print('Terminating bot')
+    logger.info('Terminating')
+
     os.kill(os.getpid(), signal.SIGINT)
---
+++ telegram_interface.py
@@ -0,0 +1,42 @@
+from connectivity_bot.telegram_wrapper import TelegramBot, TelegramMessageFormat, asynchandler
+from telegram.update import Update
+from telegram.ext.callbackcontext import CallbackContext
+from telegram.ext.commandhandler import CommandHandler
+from telegram.ext.messagehandler import MessageHandler
+from telegram.ext.filters import Filters
+
+import pandas as pd
+from dataclasses import asdict
+import logging
+
+logger = logging.getLogger('pararius')
+
+def init_telegram_bot(token, chat_id):
+    desc_padding = '                '
+    body_padding = '\n        '
+
+    TelegramBot.setToken(token)
+    TelegramBot.setChatId(chat_id)
+    TelegramBot.add_handler(CommandHandler('kill', TelegramBot.kill), description="Killing the bot...")
+    TelegramBot.add_handler(CommandHandler('help', TelegramBot.help), description="Print help message")
+    TelegramBot.add_handler(CommandHandler('ping', TelegramBot.pong), description="Ping the bot")
+    TelegramBot.add_handler(MessageHandler(Filters.text, unknown_command))
+
+    new_advert_format = TelegramMessageFormat('New advert found','',body_padding,'{}: {}')
+    TelegramBot.add_format('new_advertisement',new_advert_format)
+
+    format = TelegramMessageFormat('Help:','\n',body_padding,'/{}: {}')
+    TelegramBot.add_format('help', format, constant_data=TelegramBot.command_desc)
+
+    format = TelegramMessageFormat('Error occured:','\n','','{}')
+    TelegramBot.add_format('error', format)
+
+
+def start_telegram_bot():
+    TelegramBot.updater.start_polling()
+    TelegramBot.send_raw_message("Pararius bot started")
+
+
+def unknown_command(update: Update, context: CallbackContext):
+    update.message.reply_text(
+        "Sorry '%s' is not a valid command" % update.message.text)
